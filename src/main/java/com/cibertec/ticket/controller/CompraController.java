ca25c6547330124c0d9900d2c3fd0840076fb570
AndreQ1597 <I202333100@cibertec.edu.pe>
2025-07-02 20:26:11 -0500

    modifique seguridad

--- src/main/java/com/cibertec/ticket/controller/CompraController.java
+++ src/main/java/com/cibertec/ticket/controller/CompraController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +24,7 @@ import com.cibertec.ticket.service.CompraService;
 @RestController
 @RequestMapping("/api/compra")
 public class CompraController {
+	
 	
 	@Autowired
 	private CompraService service;
@@ -43,6 +46,11 @@ public class CompraController {
 	@PostMapping
 	@PreAuthorize("isAuthenticated()")
 	public ResponseEntity<String> saveCompra(@RequestBody CompraRequestDTO compraDTO) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+	    System.out.println("Usuario autenticado: " + auth.getName());
+	    System.out.println("Roles del usuario: " + auth.getAuthorities());
+
 	    service.saveCompra(compraDTO);
 	    return ResponseEntity.status(HttpStatus.CREATED).body("Compra realizada correctamente.");
 	}
